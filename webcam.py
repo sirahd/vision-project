ea07f4090167c513e040ff0356fcdf8c0f1e13ff
sirahd <sirah@uw.edu>
2018-06-08 15:05:00 -0700

    done

--- webcam.py
+++ webcam.py
@@ -2,15 +2,31 @@ import models
 import numpy as np
 import cv2
 import torch
+import math
 from torchvision import transforms
 
 def model():
-    m = models.CoolNet()
-    m.load_state_dict(torch.load('model/CoolNetFinal'))
+    model_path = 'model/CoolNetFinal'
+    if model_path == 'model/CoolNetFinal':
+        m = models.CoolNet()
+    else:
+        m = models.VGG()
+    m.load_state_dict(torch.load(model_path, map_location='cpu'))
     return m
+
+def mask_image(im):
+    r = 128
+    for y in range(im.shape[1]):
+        for x in range(im.shape[0]):
+            xn = x-128
+            yn = y-128
+            if math.sqrt(xn*xn + yn*yn) >= r:
+                im[x][y] = [255, 255, 255]
 
 def transform(im):
     im = cv2.resize(im, (256, 256))
+    #mask_image(im)
+    #cv2.imwrite('face.jpg', im)
     t = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -19,7 +35,7 @@ def transform(im):
 
 def show_label(im, t, loc):
     t = t.view(-1).item()
-    print(t)
+    #print(t)
     p = "Male" if t >= 0 else "Female"
     args = [im, p, loc, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2]
     cv2.putText(*args)
@@ -27,8 +43,8 @@ def show_label(im, t, loc):
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
     print(cap.get(3), cap.get(4))
-    #cap.set(3, 640)
-    #cap.set(4, 360)
+    cap.set(3, 640)
+    cap.set(4, 360)
     cascPath = 'lbpcascade_frontalface.xml'
     #cascPath = 'haar.xml'
     faceCascade = cv2.CascadeClassifier(cascPath)
@@ -45,7 +61,7 @@ if __name__ == "__main__":
         faces = faceCascade.detectMultiScale(gray, 1.1, 5, minSize=(20, 20))
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            subimage = frame[x:x+w, y:y+h]
+            subimage = frame[y:y+h, x:x+w]
             if subimage.size == 0:
                 continue
             show_label(frame, m(transform(subimage)), (x, max(y - 5, 0)))
